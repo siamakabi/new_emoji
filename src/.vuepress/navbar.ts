a2ec7f82b43725cffdecd476b7e4a33484e824c3
siamakabi <siamakabiari@gmail.com>
2024-02-08 19:31:18 +0330

    new

---
+++ src/.vuepress/navbar.ts
@@ -0,0 +1,11 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  "/",
+  
+  // {
+  //   text: "ایموجی ها",
+  //   icon: "book",
+  //   link: "gg",
+  // },
+]);
---
+++ src/.vuepress/sidebar.ts
@@ -0,0 +1,16 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export default sidebar({
+  "/": [
+   
+    // {
+    //   text: "ایموجی ها",
+    //   icon: "laptop-code",
+    //   prefix: "/ایموجی-ها/",
+    //   link: "/ایموجی-ها/",
+    //   children: "structure",
+    // },
+   
+  ],
+});
+
